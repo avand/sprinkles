c3a3c00b58502b3f75e1ac51d1b0f18e693b8f38
Stephen Korecky <skorecky@gmail.com>
2014-10-20 16:32:36 -0500

    adding  to object prototype with spec

--- src/object_extensions.js
+++ src/object_extensions.js
@@ -3,3 +3,14 @@ Object.prototype.$forEach = function(callback) {
     if (this.hasOwnProperty(key)) callback(key, this[key]);
   }
 };
+
+Object.prototype.$try = function(method, args) {
+  method = this[method];
+  if (method === undefined) {
+    return null;
+  } else if (typeof method === 'function') {
+    return method.apply(null, args);
+  } else {
+    return method;
+  }
+};
--- test/object_extensions_test.js
+++ test/object_extensions_test.js
@@ -25,3 +25,40 @@ test("$forEach", function() {
   deepEqual(keys,   ["a", "b", "c"]);
   deepEqual(values, [1, 2, 3]);
 });
+
+test("$try", function() {
+  var cbTest = function() {
+    return "test";
+  }
+
+  var object = {
+    foo: function() {
+      return "Foo";
+    },
+    bar: {
+      taz: "Bar Taz"
+    },
+    qux: function(foo, callback) {
+      if(typeof(callback) === "function") {
+        return callback();
+      } else {
+        return foo
+      }
+    }
+  };
+
+  // It should call and return a object function
+  deepEqual(object.$try('foo'), "Foo");
+  // It should return an child object
+  deepEqual(typeof(object.$try('bar')), "object");
+  // It should call and return a object string
+  deepEqual(object.$try('bar').$try('taz'), "Bar Taz");
+  // It should call and if method doesn't exist return null
+  deepEqual(object.$try('bar').$try('test'), null);
+  // It should pass args as expected
+  deepEqual(object.$try('qux', ["foo"]), "foo");
+  // It should call a function
+  deepEqual(object.$try('qux', ["foo", cbTest]), "test");
+  // It should return a callback function
+  deepEqual(object.$try('qux', ["foo", function(){return "test2"}]), "test2");
+});
