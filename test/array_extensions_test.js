26cd299d6d6826d9c264847414e69a0ec3168e96
Avand Amiri <avand@avandamiri.com>
2016-07-15 15:04:38 -0700

    Rewriting array extensions test

--- test/array_extensions_test.js
+++ test/array_extensions_test.js
@@ -1,28 +1,38 @@
-module("array extensions");
+describe("Array.prototype", function() {
+  describe("$flatten()", function() {
+    it("returns a one-dimensional flattening of the array", function() {
+      assert.deepEqual([1, 2, 3].$flatten(), [1, 2, 3]);
+      assert.deepEqual([[1], [2], [3]].$flatten(), [1, 2, 3]);
+      assert.deepEqual([[1, 2], [3]].$flatten(), [1, 2, 3]);
+      assert.deepEqual([[1], [[2]], [3]].$flatten(), [1, [2], 3])
+    });
+  });
 
-test("$flatten", function() {
-  deepEqual([1, 2, 3].$flatten(),         [1, 2, 3]);
-  deepEqual([[1], [2], [3]].$flatten(),   [1, 2, 3]);
-  deepEqual([[1, 2], [3]].$flatten(),     [1, 2, 3]);
-  deepEqual([[1], [[2]], [3]].$flatten(), [1, [2], 3]);
-});
+  describe("$groupBy()", function() {
+    it("returns an empty object for an empty array", function() {
+      assert.deepEqual([].$groupBy(function(n) {
+        return n;
+      }), {});
+    });
 
-test("$groupBy", function() {
-  deepEqual([].$groupBy(function(n) {
-    return n;
-  }), {});
+    it("groups objects under undefined", function() {
+      assert.deepEqual(["cat", "dog", "turtle"].$groupBy(function(n) {
+        return undefined;
+      }), { undefined: ["cat", "dog", "turtle"] });
+    });
 
-  deepEqual(["cat", "dog", "turtle"].$groupBy(function(n) {
-    return undefined;
-  }), { undefined: ["cat", "dog", "turtle"] });
+    it("groups objects by a specified key", function() {
+      assert.deepEqual(["cat", "dog", "turtle"].$groupBy(function(animal) {
+        return animal.length;
+      }), { 3: ["cat", "dog"], 6: ["turtle"] });
+    });
+  });
 
-  deepEqual(["cat", "dog", "turtle"].$groupBy(function(animal) {
-    return animal.length;
-  }), { 3: ["cat", "dog"], 6: ["turtle"] });
-});
-
-test("$includes", function() {
-  deepEqual([1, 2, 'a', 'b', true].$includes('b'), true);
-  deepEqual([1, 2, 'a', 'b', true].$includes('d'), false);
-  deepEqual([1, 2, 'a', 'b', true].$includes(true), true);
+  describe("$includes()", function() {
+    it("returns true if the object exists in the array", function() {
+      assert.isTrue([1, 2, 'a', 'b', true].$includes('b'));
+      assert.isFalse([1, 2, 'a', 'b', true].$includes('d'));
+      assert.isTrue([1, 2, 'a', 'b', true].$includes(true));
+    });
+  });
 });
