fb7f9d0da684770ee5c584afb41895edd5f2abc2
Avand Amiri <avand@avandamiri.com>
2016-07-15 15:44:05 -0700

    Rewriting node extensions text

--- test/node_extensions_test.js
+++ test/node_extensions_test.js
@@ -1,21 +1,16 @@
-module("node extensions");
+describe("Node.prototype", function() {
+  describe("$removeChildren()", function() {
+    it("should remove all descendant nodes", function() {
+      var parent     = document.createElement("ul"),
+          child      = document.createElement("li"),
+          grandchild = document.createTextNode("foo");
 
-test("$removeChildren", function() {
-  var parent     = document.createElement("ul"),
-      child      = document.createElement("li"),
-      sibling    = document.createElement("li"),
-      grandchild = document.createTextNode("foo");
+      child.appendChild(grandchild);
+      parent.appendChild(child);
 
-  child.appendChild(grandchild);
-  parent.appendChild(child);
-  parent.appendChild(sibling);
+      parent.$removeChildren();
 
-  equal(parent.childNodes.length, 2);
-  equal(child.childNodes.length,  1);
-
-  parent.$removeChildren();
-  child.$removeChildren();
-
-  equal(parent.childNodes.length, 0);
-  equal(child.childNodes.length,  0);
+      assert.equal(parent.childNodes.length, 0);
+    })
+  });
 });
