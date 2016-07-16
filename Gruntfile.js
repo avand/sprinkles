59b7a93da44f07cace55d3c6bd720f1a60037c60
Avand Amiri <avand@avandamiri.com>
2016-07-15 17:51:28 -0700

    Cleaning up package.json and Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,20 +10,8 @@ module.exports = function(grunt) {
         dest: "dist/<%= pkg.name %>-<%= pkg.version %>.js"
       }
     },
-    connect: {
-      server: {
-        options: { port: "8000", base: "." }
-      }
-    },
     jshint: {
       files: ["Gruntfile.js", "src/**/*.js", "test/*.js"],
-    },
-    qunit: {
-      all: {
-        options: {
-          urls: ["http://localhost:8000/test/index.html"]
-        }
-      }
     },
     uglify: {
       dist: {
@@ -35,12 +23,9 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks("grunt-contrib-concat");
-  grunt.loadNpmTasks("grunt-contrib-connect");
   grunt.loadNpmTasks("grunt-contrib-jshint");
-  grunt.loadNpmTasks("grunt-contrib-qunit");
   grunt.loadNpmTasks("grunt-contrib-uglify");
 
-  grunt.registerTask("default", ["test", "concat", "uglify"]);
-  grunt.registerTask("build",   ["jshint", "concat", "uglify"]);
-  grunt.registerTask("test",    ["jshint", "connect", "qunit"]);
+  grunt.registerTask("default", ["concat", "uglify"]);
+  grunt.registerTask("build", ["jshint", "concat", "uglify"]);
 };
