7cfb869d496bed9e179874922929a836029b6980
Avand Amiri <avand@avandamiri.com>
2016-07-21 17:09:05 -0700

    Making Object extensions non-enumerable

--- src/js/object_extensions/object.js
+++ src/js/object_extensions/object.js
@@ -1,17 +1,25 @@
-Object.prototype.$forEach = function(callback) {
-  for (var key in this) {
-    if (this.hasOwnProperty(key)) callback(key, this[key]);
-  }
-};
+Object.defineProperties(Object.prototype, {
+  $forEach: {
+    enumerable: false,
+    value: function(callback) {
+      for (var key in this) {
+        if (this.hasOwnProperty(key)) callback(key, this[key]);
+      }
+    }
+  },
 
-Object.prototype.$try = function() {
-  var args = Array.prototype.slice.call(arguments);
-  var method = this[args.shift()];
-  if (method === undefined) {
-    return;
-  } else if (typeof method === 'function') {
-    return method.apply(null, args);
-  } else {
-    return method;
+  $try: {
+    enumerable: false,
+    value: function() {
+      var args = Array.prototype.slice.call(arguments);
+      var method = this[args.shift()];
+      if (method === undefined) {
+        return;
+      } else if (typeof method === 'function') {
+        return method.apply(null, args);
+      } else {
+        return method;
+      }
+    }
   }
-};
+});
